a9cf0bd594bfcbb9eb57770778618bf4aa214e9c
Mia Kuon <mia.kuon@gmail.com>
2024-02-03 23:08:15 +0300

    dmenu patch&customization:    - centered    - vertical    - with "Execute:" prepended

--- config.def.h
+++ config.def.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", gray2, "-nf", white, "-sb", accent, "-sf", gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-c", "-l", 10, "-p", "Execute:", "-m", dmenumon, "-fn", dmenufont, "-nb", gray2, "-nf", white, "-sb", accent, "-sf", gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 static const char *obsdcmd[]  = { "obsidian", NULL };
 static const char *fmcmd[] = { "spacefm", NULL };
